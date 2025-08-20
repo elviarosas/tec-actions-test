16f9a9358fb287df917993fb463ece2354d78a1a
Elvia Rosas <70449140+elviarosas@users.noreply.github.com>
2025-08-19 19:36:49 -0600

    fix: usar PORT de Railway

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
     return jsonify({"message": f"it works, {who}!"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))  # toma PORT de Railway, o 3000 si corres local
+    port = int(os.environ.get("PORT", 3000))  # Railway te da PORT dinámico
     app.run(host="0.0.0.0", port=port)
